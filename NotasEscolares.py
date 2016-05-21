68fca5c34776d6d256c07d2726524d187230ecd3
Gessiel2002 <Xuserzinho@gmail.com>
2016-05-20 23:04:19 -0300

    Update NotasEscolares.py

--- NotasEscolares.py
+++ NotasEscolares.py
@@ -10,8 +10,8 @@ print("")
 
 Bimestre1 float(input(" Resultado do 1º Bimestre, ")
 Bimestre2 float(input(" Resultado do 2º Bimestre, ")
-Bimestre3 float(input(" resultado do 3º Bimestre, ")
-Bimestre4 float(input(" resultado do 4º Bimestre, ")
+Bimestre3 float(input(" Resultado do 3º Bimestre, ")
+Bimestre4 float(input(" Resultado do 4º Bimestre, ")
 
 print("")
 
@@ -21,4 +21,11 @@ print("")
 
 print(" Você ficou com: ", + Valores, + " Pontos... ")
 
-pause
+if(Valores >= 60):
+                print(" Parabéns você foi aprovado... ")
+else:
+                if(Valores <= 59.9):
+                print(" Você foi reprovado... ")
+else:
+
+Sair = input(" Pressione qualquer tecla para sair... ")
