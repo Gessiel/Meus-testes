fd6e08eb61157ef8fa084e80f5db9efdcb545da9
Gessiel2002 <Xuserzinho@gmail.com>
2016-05-20 22:55:40 -0300

    Notas Escolares.py
    Programa simples que soma suas notas escolares e diz se você foi aprovado ou reprovado...

---
+++ NotasEscolares.py
@@ -0,0 +1,24 @@
+#
+# Xuser Produções
+#
+
+#coding UTF-8
+
+print(" Notas Escolares.... ")
+
+print("")
+
+Bimestre1 float(input(" Resultado do 1º Bimestre, ")
+Bimestre2 float(input(" Resultado do 2º Bimestre, ")
+Bimestre3 float(input(" resultado do 3º Bimestre, ")
+Bimestre4 float(input(" resultado do 4º Bimestre, ")
+
+print("")
+
+Valores = Bimestre1 + Bimestre2 + Bimestre3 + Bimestre4
+
+print("")
+
+print(" Você ficou com: " + Valores + " Pontos... ")
+
+pause
