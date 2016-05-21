a1ce1cf6940c58ce5267e3f50e9e37c2692a6b5c
Gessiel2002 <Xuserzinho@gmail.com>
2016-05-21 13:16:15 -0300

    Update NotasEscolares.py

--- NotasEscolares.py
+++ NotasEscolares.py
@@ -1,6 +1,4 @@
-#
 # Xuser Produções
-#
 
 #coding UTF-8
 
@@ -8,24 +6,31 @@ print(" Notas Escolares.... ")
 
 print("")
 
-Bimestre1 float(input(" Resultado do 1º Bimestre, ")
-Bimestre2 float(input(" Resultado do 2º Bimestre, ")
-Bimestre3 float(input(" Resultado do 3º Bimestre, ")
-Bimestre4 float(input(" Resultado do 4º Bimestre, ")
-
+Bimestre1 = float(input(" Resultado do 1º Bimestre, "))
+print(" Obrigado...")              
+Bimestre2 = float(input(" Resultado do 2º Bimestre, "))
+print(" Obrigado...")             
+Bimestre3 = float(input(" Resultado do 3º Bimestre, "))
+print(" Obrigado...")              
+Bimestre4 = float(input(" Resultado do 4º Bimestre, "))
+print(" Obrigado...")
+                   
 print("")
 
 Valores = Bimestre1 + Bimestre2 + Bimestre3 + Bimestre4
 
 print("")
 
-print(" Você ficou com: ", + Valores, + " Pontos... ")
+print(" Você ficou com: %.2f" %Valores," Pontos..." )
 
 if(Valores >= 60):
+                print("")
                 print(" Parabéns você foi aprovado... ")
 else:
                 if(Valores <= 59.9):
-                print(" Você foi reprovado... ")
-else:
+                    print("")
+                    print(" Você foi reprovado... ")
+
+print("")
 
 Sair = input(" Pressione qualquer tecla para sair... ")
